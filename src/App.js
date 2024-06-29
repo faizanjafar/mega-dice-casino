71aec7ada60591668e2f80b4213d3897c4e408cb
faizanjafar <mohammadfazan624@gmail.com>
2024-06-29 18:13:09 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="bg-gray-100 p-4">
+      <h1 className="text-2xl font-bold text-gray-800">Hello, Tailwind CSS!</h1>
+      <p className="text-gray-600 mt-2">
+        This is a React app styled with Tailwind CSS.
+      </p>
+    </div>
+  );
+}
+
+export default App;
